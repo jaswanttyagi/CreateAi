6274fd5662f05da0577a942a1bd851a81f79d9c0
jaswant singh <js1178052@gmail.com>
2026-03-14 11:00:32 +0530

    push

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,6 @@ if (isProduction) {
 
 app.disable("x-powered-by");
 app.use(cors(corsOptionsDelegate));
-app.options("*", cors(corsOptionsDelegate));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
